b3ce7d30ee0839ec174c99ff6faf162fffbccaa7
Abhimanyu Dwivedi <abhimanyudwivedi@outlook.com>
2023-06-23 00:19:58 +0530

    Inital

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Jenkins Demo!
+        </p>
+        <p>
+          This is an inital code.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
